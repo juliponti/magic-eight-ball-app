e6b3bf346e6bcf43700594a04d0c0b0a2f63f831
Julieta <julieta.pontino@gmail.com>
2022-02-23 21:04:06 -0300

    add responsive

--- src/App.js
+++ src/App.js
@@ -55,18 +55,18 @@ function App() {
   return (
     <div className="bg-desktop-home bg-cover bg-bottom min-h-[100vh] flex flex-col justify-center items-center">
       <header className="w-full py-6">
-        <h1 className="font-cursive text-6xl text-black w-fit py-5 mx-auto capitalize">
+        <h1 className="text-[45px] font-cursive md:text-6xl text-black w-fit py-5 mx-auto capitalize">
           Magic eight ball
         </h1>
       </header>
-      <main className="w-full flex flex-col justify-center items-center">
+      <main className="px-5 md:p-0 w-full flex flex-col justify-center items-center">
         <form className="relative mx-auto my-5 flex flex-col justify-center">
-          <label className="font-sanserif text-2xl text-white self-center p-5">
+          <label className="text-[20px] font-sanserif md:text-2xl text-white self-center p-5">
             Ask a yes or no question
           </label>
           <input
             type="text"
-            className="w-96 h-8 px-2 focus:outline-indigo-700 text-2xl"
+            className="w-[18rem] md:w-96 h-8 px-2 focus:outline-indigo-700 text-2xl"
             value={userInput}
             onChange={handleChange}
           />
@@ -84,17 +84,17 @@ function App() {
         <div className="w-full">
           <div
             ref={ball}
-            className="w-[26rem] h-[26rem] bg-black bg-gradient-to-r from-gray-800 to-black rounded-full mt-8 mx-auto"
+            className="w-[17rem] h-[17rem] md:w-[26rem] md:h-[26rem] bg-black bg-gradient-to-r from-gray-800 to-black rounded-full mt-8 mx-auto"
           >
             <div className="h-full w-full flex justify-center items-center">
-              <div className="w-[14rem] h-[14rem] rounded-full bg-gradient-to-r from-gray-800 to-black overflow-hidden">
+              <div className="w-[10rem] h-[10rem] md:w-[14rem] md:h-[14rem] rounded-full bg-gradient-to-r from-gray-800 to-black overflow-hidden">
                 <div
                   ref={prediction}
-                  className=" hidden relative bottom-[-58px] left-[-8px] border-l-[180px] border-l-indigo-700 border-t-[100px] border-b-[100px] border-y-transparent rotate-[96deg] skew-y-1 drop-shadow-3xl "
+                  className=" hidden relative bottom-[-34px] left-[1px] border-t-[70px] border-b-[70px] border-l-[150px] md:bottom-[-58px] md:left-[-8px] md:border-l-[180px] border-l-indigo-700 md:border-t-[100px] md:border-b-[100px] border-y-transparent rotate-[96deg] skew-y-1 drop-shadow-3xl "
                 >
                   <h3
-                    className="absolute
-                  top-[-47px] right-[116px] w-[107px] font-mono text-xl rotate-[270deg] uppercase"
+                    className="absolute w-[85px] right-[68px] top-[-34px] text-center
+                  md:top-[-47px] md:right-[116px] md:w-[107px] md:text-left font-mono text-xl rotate-[270deg] uppercase"
                   >
                     {answer}
                   </h3>
